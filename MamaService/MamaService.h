fefafa17009fc9a83b7811309315fa56f9ecc185
Mr.Robot <1028813178@qq.com>
2023-05-31 20:51:05 +0800

    鸡你太美

---
+++ MamaService/MamaService.h
@@ -0,0 +1,13 @@
+//
+// Created by 刘震 on 2023/5/31.
+//
+
+#ifndef CODE_MAMASERVICE_H
+#define CODE_MAMASERVICE_H
+
+class MamaService {
+public:
+    MamaService();
+};
+
+#endif //CODE_MAMASERVICE_H
