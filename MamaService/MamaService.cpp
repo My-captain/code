0b89302892bb6d8683c191ccf1dcdcb919168c89
Mr.Robot <1028813178@qq.com>
2023-05-31 21:04:18 +0800

    鸡你太美

--- MamaService/MamaService.cpp
+++ MamaService/MamaService.cpp
@@ -2,9 +2,38 @@
 // Created by 单分子电子学 on 2023/5/30.
 //
 
-#include<bits/stdc++.h>
-#include"ModelSliceReader.h"
+#include "MamaService.h"
 using namespace std;
+
+bool MamaService::downloadFromHDFS(std::string &hdfs_path, std::string &local_path) {
+    return false;
+}
+
+MamaService::MamaService() {
+
+}
+
+MamaService::~MamaService() {
+
+}
+
+bool MamaService::registerETCD() {
+    return false;
+}
+
+bool MamaService::pushToETCD(string &key, string &value) {
+    return false;
+}
+
+std::string MamaService::pullFromETCD() {
+    return std::string();
+}
+
+bool MamaService::getCurrentVersion(string &hdfs_path) {
+    this->currentVersion =  "current";  // hdfs读取当前版本;
+    std::vector<thread> downloadThreads;
+    return false;
+}
 
 int main() {
 	vector<ModelSliceReader> A(3);
--- MamaService/MamaService.h
+++ MamaService/MamaService.h
@@ -1,13 +1,38 @@
 //
 // Created by 刘震 on 2023/5/31.
 //
-
+#pragma once
+#include <string>
+#include<bits/stdc++.h>
+#include"ModelSliceReader.h"
 #ifndef CODE_MAMASERVICE_H
 #define CODE_MAMASERVICE_H
+using namespace std;
 
 class MamaService {
+private:
+    std::string currentVersion;		// 当前对外提供服务的版本
+    std::string newVersion;			// 当前从hdfs拉取的最新版本（可能是最新，也肯能是回滚）
+    std::map<std::string, ModelSliceReader> modelReaderMap;
+//    BlockingQueue<...> modelData; // 消费者线程池(负责下载)
 public:
     MamaService();
+    ~MamaService();
+    // 向ETCD 注册服务
+    bool registerETCD();
+
+    // 提供模型数据读取，拿不到就阻塞
+//    char[] Get(...) {}
+
+    // 失败情况下不断自旋重试
+    bool downloadFromHDFS(std::string& hdfs_path, std::string& local_path);
+
+    // 发送通知
+    bool pushToETCD(std::string& key, std::string &value);
+
+    std::string pullFromETCD();
+
+    bool getCurrentVersion(std::string& hdfs_path);
 };
 
 #endif //CODE_MAMASERVICE_H
